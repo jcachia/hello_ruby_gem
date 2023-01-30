c8cc1903c1c83ee302b00cb2eaadece78b75a31e
Jason C <jason.cachia@care.com>
2023-01-30 15:50:19 -0500

    init hello ruby gem

---
+++ lib/hello_ruby_gem.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "hello_ruby_gem/version"
+
+module HelloRubyGem
+  class Error < StandardError; end
+
+  class Hello
+    def say_it(name="Beautiful")
+      return "Hello there, #{name}"
+    end
+  end
+
+end
---
+++ lib/hello_ruby_gem/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module HelloRubyGem
+  VERSION = "0.1.0"
+end
